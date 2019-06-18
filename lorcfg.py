53e5635e0628574fb8ffe0af8f6cb24d73abb720
shkolnick-kun <shkolnick.kun@gmail.com>
2019-06-18 20:47:09 +0500

    Create lorcfg.py

---
+++ lorcfg.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 25 23:44:39 2019
+@author: anon
+"""
+
+LOGIN  = 'user'
+PASS = 'pass'
+REPORT_TO = '15041564'
+
+OUT_FILE = 'log.txt'
