5113eb89323cd05df5bec883d8452b841ca5532a
shkolnick-kun <shkolnick.kun@gmail.com>
2019-06-18 22:35:17 +0500

    Update post_to_lor.py

--- post_to_lor.py
+++ post_to_lor.py
@@ -19,27 +19,6 @@
 
     Please contact with me by E-mail: shkolnick.kun@gmail.com
 """
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-    This file is a part of the lor_neuro_rat project.
-    Copyright (C) 2019 anonimous
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-    Please contact with me by E-mail: shkolnick.kun@gmail.com
-"""
 from io import StringIO
 from json import JSONDecoder#, JSONEncoder
 import pickle as pk
