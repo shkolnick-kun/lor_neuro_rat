89a25af22a03f163db2fa7d6b3f77cb84200223d
shkolnick-kun <shkolnick.kun@gmail.com>
2019-07-10 11:54:15 +0500

    Changed Readme.md. Added license clarification in notebooks.

--- mdl_server.py
+++ mdl_server.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+    This file is a part of the lor_neuro_rat project.
+    Copyright (C) 2019 anonimous
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Please contact with me by E-mail: shkolnick.kun@gmail.com
+"""
 from enum import Enum
 import pickle as pk
 import struct as st
@@ -213,6 +232,15 @@ async def gateway_factory():
     #
     loop = asyncio.get_event_loop()
     #Для работы tf с multiporcessing нужна "магия", пока не осилил.
+    # Как вариант, можно определить все, что связано с моделями в отдельном
+    # модуле и импортировать его уже в форкнутом процессе. 
+    #
+    # Тогда вся рабта с моделями будет происходить в одном единственном процессе, 
+    # и "магия" не понядобится, понадибится только перенаправить 
+    # sdtout и sdterr в Pipe/Queue, ну и RPC тоже сделать через Pipe/Queue.
+    #
+    # В этом случае на асинхронной стороне можно будет сделать связь 
+    # с помощью ThreadPoolExecutor и этих самых Pipe/Queue
     #executor = concurrent.futures.ProcessPoolExecutor(max_workers=1)
     executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
     #
